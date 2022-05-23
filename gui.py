714ec673b50606855d6506b3700030c86c666688
Matt <mjhicks0013@gmail.com>
2022-05-23 22:58:49 +1000

    upload an image basic set

--- gui.py
+++ gui.py
@@ -1,15 +1,54 @@
 from tkinter import *
-
-# create root window
-
+from tkinter.ttk import *
+from tkinter.filedialog import askopenfile
+import time
 root = Tk()
 
-root.title("Image Analysis")
-root.geometry("350x200")
+def open_file():
+    file_path = askopenfile(mode='r', filetypes=[('Image Files', 'jpg')])
+    if file_path is not None:
+        pass
 
-# execute the gui
+
+imageText = Label(
+    root,
+    text='Upload logo with extension .jpg'
+    )
+imageText.grid(row=0, column=0, padx=10)
+
+uploadImagebtn = Button(
+    root,
+    text ='Choose File',
+    command = lambda:open_file()
+    )
+uploadImagebtn.grid(row=0, column=1)
+
+
+uploadButton = Button(
+    root,
+    text='Upload Files',
+    command = lambda:uploadFiles()
+    )
+uploadButton.grid(row=3, columnspan=3, pady=10)
+
+def uploadFiles():
+    pb1 = Progressbar(
+        root,
+        orient=HORIZONTAL,
+        length=300,
+        mode='determinate'
+        )
+    pb1.grid(row=4, columnspan=3, pady=20)
+    for i in range(5):
+        root.update_idletasks()
+        pb1['value'] += 20
+        time.sleep(1)
+    pb1.destroy()
+    Label(root, text='File Uploaded Successfully!', foreground='green').grid(row=4, columnspan=3, pady=10)
+
 root.mainloop()
-#comment
 
-if "state" == 'confused':
-    print("Help!")
+
+
+
+
