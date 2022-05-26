5a72e3483f1c9600eeeb49c5f0a801f3f278f9d3
AstralShard <sammyneumann18@gmail.com>
2022-05-26 14:58:44 +1000

    Colour Identification!

--- autoML.py
+++ autoML.py
@@ -1,3 +1,5 @@
+import time
+
 #### Text Compare ####
 
 from sentence_transformers import SentenceTransformer, util
@@ -105,7 +107,73 @@ def run_general_image_compare():
 
     print(f'Score: {output}')
 
+#### Colour Identification ####
+
+def identify_colours(path, number_of_colours):
+    print(time.time())
+    from cv2 import kmeans
+    import matplotlib
+    import pandas as pd
+    import numpy as np
+    import matplotlib.pyplot as plt
+    import sklearn
+    from sklearn.cluster import KMeans
+    from collections import Counter
+    from skimage.color import rgb2lab, deltaE_cie76
+    import cv2
+
+    plt.style.use('ggplot')
+    plt.rcParams['font.family'] = 'sans-serif'
+    plt.rcParams['font.serif'] = 'Ubuntu'
+    plt.rcParams['font.monospace'] = 'Ubuntu Mono'
+    plt.rcParams['font.size'] = 14
+    plt.rcParams['axes.labelsize'] = 12
+    plt.rcParams['axes.labelweight'] = 'bold'
+    plt.rcParams['axes.titlesize'] = 12
+    plt.rcParams['xtick.labelsize'] = 12
+    plt.rcParams['ytick.labelsize'] = 12
+    plt.rcParams['legend.fontsize'] = 12
+    plt.rcParams['figure.titlesize'] = 12
+    plt.rcParams['image.cmap'] = 'jet'
+    plt.rcParams['image.interpolation'] = 'none'
+    plt.rcParams['figure.figsize'] = (10,10)
+    plt.rcParams['axes.grid'] = False
+    plt.rcParams['lines.linewidth'] = 2
+    plt.rcParams['lines.markersize'] = 8
+    colors = ['xkcd:pale orange', 'xkcd:sea blue', 'xkcd:pale red', 'xkcd:sage green', 'xkcd:terra cotta', 'xkcd:dull purple', 'xkcd:teal', 'xkcd: goldenrod', 'xkcd:cadet blue',
+    'xkcd:scarlet']
+
+    def get_image(image_path):
+        image = cv2.imread(image_path)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        return image
+    def RGB2HEX(color):
+        return "#{:02x}{:02x}{:02x}".format(int(color[0]), int(color[1]), int(color[2]))
+    
+    image = get_image(path)
+    
+    modified_image = image.reshape(image.shape[0]*image.shape[1], 3)
+    clf = KMeans(n_clusters=number_of_colours)
+    labels = clf.fit_predict(modified_image)
+
+    counts = Counter(labels)
+
+    center_colours = clf.cluster_centers_
+    # We get order colours by iterating htorugh the keys
+    ordered_colours = [center_colours[i] for i in counts.keys()]
+    hex_colours = [RGB2HEX(ordered_colours[i]) for i in counts.keys()]
+    rgb_colours = [ordered_colours[i] for i in counts.keys()]
+
+    plt.title('Colour Detection ($n=10$)', fontsize=20)
+    plt.pie(counts.values(), labels = hex_colours, colors = hex_colours)
+    print(time.time())
+    plt.show()
+    
+
+def run_colour_identification():
+    identify_colours(r'C:\Users\sammy.LAPTOP-RUR693FV\Pictures\Windbound\13_2_2022__19_50_58.png', 5)
+
 #### Testing ####
 
 if __name__ == '__main__':
-    run_general_image_compare()
+    run_colour_identification()
--- textComparisonTest.py
+++ textComparisonTest.py
@@ -1,15 +1,56 @@
-from sentence_transformers import SentenceTransformer, util
-from PIL import Image
+from cv2 import kmeans
+import matplotlib
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import sklearn
+from sklearn.cluster import KMeans
+from collections import Counter
+from skimage.color import rgb2lab, deltaE_cie76
+import cv2
 
-#Load CLIP model
-model = SentenceTransformer('clip-ViT-B-32')
+plt.style.use('ggplot')
+plt.rcParams['font.family'] = 'sans-serif'
+plt.rcParams['font.serif'] = 'Ubuntu'
+plt.rcParams['font.monospace'] = 'Ubuntu Mono'
+plt.rcParams['font.size'] = 14
+plt.rcParams['axes.labelsize'] = 12
+plt.rcParams['axes.labelweight'] = 'bold'
+plt.rcParams['axes.titlesize'] = 12
+plt.rcParams['xtick.labelsize'] = 12
+plt.rcParams['ytick.labelsize'] = 12
+plt.rcParams['legend.fontsize'] = 12
+plt.rcParams['figure.titlesize'] = 12
+plt.rcParams['image.cmap'] = 'jet'
+plt.rcParams['image.interpolation'] = 'none'
+plt.rcParams['figure.figsize'] = (10,10)
+plt.rcParams['axes.grid'] = False
+plt.rcParams['lines.linewidth'] = 2
+plt.rcParams['lines.markersize'] = 8
+colors = ['xkcd:pale orange', 'xkcd:sea blue', 'xkcd:pale red', 'xkcd:sage green', 'xkcd:terra cotta', 'xkcd:dull purple', 'xkcd:teal', 'xkcd: goldenrod', 'xkcd:cadet blue',
+'xkcd:scarlet']
 
-#Encode an image:
-img_emb = model.encode(Image.open(r'C:\Users\sammy.LAPTOP-RUR693FV\Pictures\Picture1.jpg'))
+def get_image(image_path):
+    image = cv2.imread(image_path)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    return image
+def RGB2HEX(color):
+    return "#{:02x}{:02x}{:02x}".format(int(color[0]), int(color[1]), int(color[2]))
+ 
+image = get_image(r'C:\Users\sammy.LAPTOP-RUR693FV\Pictures\Picture4.jpg')
+number_of_colours = 5
+modified_image = image.reshape(image.shape[0]*image.shape[1], 3)
+clf = KMeans(n_clusters=number_of_colours)
+labels = clf.fit_predict(modified_image)
 
-#Encode text descriptions
-img_emb_2 = model.encode(Image.open(r'C:\Users\sammy.LAPTOP-RUR693FV\Pictures\Picture3.jpg'))
+counts = Counter(labels)
 
-#Compute cosine similarities 
-cos_scores = util.cos_sim(img_emb, img_emb_2)
-print(cos_scores)
+center_colours = clf.cluster_centers_
+# We get order colours by iterating htorugh the keys
+ordered_colours = [center_colours[i] for i in counts.keys()]
+hex_colours = [RGB2HEX(ordered_colours[i]) for i in counts.keys()]
+rgb_colours = [ordered_colours[i] for i in counts.keys()]
+
+plt.title('Colour Detection ($n=10$)', fontsize=20)
+plt.pie(counts.values(), labels = hex_colours, colors = hex_colours)
+plt.show()
