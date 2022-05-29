2e737049ebb30d40248346d8f8b225bc0c8a03fc
mhic0003 <mhic0003@student.monash.edu>
2022-05-30 08:47:08 +1000

    Added string_to_lists function to mainflow.py

--- main_flow.py
+++ main_flow.py
@@ -28,6 +28,33 @@ def main_flow():
 def exit():
     pass
 
+def string_to_lists(input_string: str) -> list:
+    individual_lists = []
+    output_list = []
+    string_to_manipulate = input_string
+    num_closed_bracket = string_to_manipulate.count("]")
+
+    for closed_bracket in range(1,num_closed_bracket):
+        if string_to_manipulate[0] == ',':
+            string_to_manipulate = string_to_manipulate[1:]
+        index_of_closed_bracket = string_to_manipulate.index(']') + 1
+        aList = string_to_manipulate[0:index_of_closed_bracket].replace(" ", "")
+        if aList[0] == '[' and aList[1] == '[':
+            aList = aList[1:]
+        individual_lists.append(aList)
+        string_to_manipulate = string_to_manipulate[index_of_closed_bracket+1:]
+
+
+    for string_list in individual_lists:
+        inner_list = []
+        string_numbers = string_list[1:-1]
+        list_of_indiv_numbers = string_numbers.split(",")
+        for number in list_of_indiv_numbers:
+            inner_list.append(float(number))
+        output_list.append(inner_list)
+
+    return output_list
+
 
 #### Main Program ####
 
