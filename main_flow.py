6561f8abb957a69294267e8003b9dbe0cd33fc2d
mhic0003 <mhic0003@student.monash.edu>
2022-05-29 14:15:01 +1000

    main program flow into functions

---
+++ main_flow.py
@@ -0,0 +1,24 @@
+
+# database pre-processing.
+def index_database():
+    pass
+
+# function for processing the 3 main pieces that make up an image, colour, shape, and text.
+def process_image(filepath: str):
+    pass
+
+# function for processing the input text.
+def process_text(text: str):
+    pass
+
+# button to show the results in a report format after image/text has been processed.
+def display_results():
+    pass
+
+# run the backbone code here for keeping the interface on and root for all decisions made on interface
+def main_flow():
+    pass
+
+# not sure if needed but exit the interface done by closing window.
+def exit():
+    pass
