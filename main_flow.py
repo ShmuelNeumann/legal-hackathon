f2d0423c5d0a9c03d0b26b675db60425ab5a0322
AstralShard <sammyneumann18@gmail.com>
2022-05-30 21:41:58 +1000

    get_image

--- main_flow.py
+++ main_flow.py
@@ -138,15 +138,15 @@ def get_results_string(isImage, results, database):
 
         outputString += "\nSimilarities To Other Layouts\n\n"
         for index in range(3):
-            outputString += f'{index+1}. Trademark No {results[0][index][0]} has a similarity of {results[0][index][1]} out of 1\n\tMore details can be found at: https://search.ipaustralia.gov.au/trademarks/search/view/{results[0][index][0]}/details' + '\n'
+            outputString += f'{index+1}. Trademark No {results[0][index][0]} has a similarity of {results[0][index][1]} out of 1\n\tMore details can be found at: https://search.ipaustralia.gov.au/trademarks/search/view/{results[0][index][0]}/details' + '\n\n'
         
         outputString += "\nSimilarities To Other Colours\n\n"
         for index in range(3):
-            outputString += f'{index+1}. Trademark No {results[1][index][0]} has a similarity of {results[1][index][1]} out of 1\n\tMore details can be found at: https://search.ipaustralia.gov.au/trademarks/search/view/{results[1][index][0]}/details' + '\n'
+            outputString += f'{index+1}. Trademark No {results[1][index][0]} has a similarity of {results[1][index][1]} out of 1\n\tMore details can be found at: https://search.ipaustralia.gov.au/trademarks/search/view/{results[1][index][0]}/details' + '\n\n'
         
         outputString += "\nSimilarities To Other Texts\n\n"
         for index in range(3):
-            outputString += f'{index+1}. Trademark No {results[2][index][0]} has a similarity of {results[2][index][1]} out of 1\n\tMore details can be found at: https://search.ipaustralia.gov.au/trademarks/search/view/{results[2][index][0]}/details' + '\n'
+            outputString += f'{index+1}. Trademark No {results[2][index][0]} has a similarity of {results[2][index][1]} out of 1\n\tMore details can be found at: https://search.ipaustralia.gov.au/trademarks/search/view/{results[2][index][0]}/details' + '\n\n'
         return outputString
     
     elif not isImage:
@@ -155,66 +155,74 @@ def get_results_string(isImage, results, database):
 
         outputString += "\nSimilarities To Other Texts\n\n"
         for index in range(3):
-            outputString += f'{index+1}. Trademark No {results[0][index][0]} has a similarity of {results[0][index][1]} out of 1\n\tMore details can be found at: https://search.ipaustralia.gov.au/trademarks/search/view/{results[0][index][0]}/details' + '\n'
+            outputString += f'{index+1}. Trademark No {results[0][index][0]} has a similarity of {results[0][index][1]} out of 1\n\tMore details can be found at: https://search.ipaustralia.gov.au/trademarks/search/view/{results[0][index][0]}/details' + '\n\n'
         
         return outputString
 
+def get_image(number:str, database)->str:
+    for index in range(len(database)):
+        if database[index].number == str(number):
+            return database[index].imagePath
 
+def main():
 
-#### Main Program ####
-
-
-print("\n====\nSearching for Preprocessed Database\n====\n")
-
-import tkinter
-import database
-
-#Check if the database needs to be indexed
-if database.checkDatabaseIndexing() == False:
-    #Index the database
-    print("\n====\nNo Preprocessed Database found\nPreprocessing Database\n====\n")
-    database.indexDatabase()
-    print("\n====\nPreprocessing Complete\n====\n")
-
-import gui
-
-print("\n====\nAccessing Database\n====\n")
-
-database = database.read_database(database.databaseToSaveLocation)
-
-print("\n====\nWaiting for User Input\n====\n")
-
-# Have the user input the image/text
-guiData = gui.initialise_tkinter()
-
-comparisonDetails = gui.getInput(guiData) #WIP - This is the function to get [isImage, text, image path]
-
-print(comparisonDetails)
-
-print("\n====\nInitialising ML functions\n====\n")
-import mlFunctions
-
-
-# If the comparison is an image one, compare the images
-if comparisonDetails[0] == True:
 
     
-    shapeResults = compare_image_shape(database, comparisonDetails[1])
+    #### Main Program ####
 
-    colourResults = compare_image_colour(database, comparisonDetails[1])
 
-    textResults = compare_image_text(database, comparisonDetails[2])
-    
-    results_string = get_results_string(True, [shapeResults, colourResults, textResults], database)
+    print("\n====\nSearching for Preprocessed Database\n====\n")
 
-  
-else:
-    textResults = compare_text(database, comparisonDetails[2])
+    import tkinter
+    import database
 
-    results_string = get_results_string(False, [textResults], database)
+    #Check if the database needs to be indexed
+    if database.checkDatabaseIndexing() == False:
+        #Index the database
+        print("\n====\nNo Preprocessed Database found\nPreprocessing Database\n====\n")
+        database.indexDatabase()
+        print("\n====\nPreprocessing Complete\n====\n")
 
-print(results_string)
-    
+    import gui
 
-gui.show_results(tkinter, results_string)
+    print("\n====\nAccessing Database\n====\n")
 
+    database = database.read_database(database.databaseToSaveLocation)
+
+    print("\n====\nWaiting for User Input\n====\n")
+
+    # Have the user input the image/text
+    guiData = gui.initialise_tkinter()
+
+    comparisonDetails = gui.getInput(guiData) #WIP - This is the function to get [isImage, text, image path]
+
+    print("\n====\nInitialising ML functions\n====\n")
+    import mlFunctions
+
+
+    # If the comparison is an image one, compare the images
+    if comparisonDetails[0] == True:
+
+        
+        shapeResults = compare_image_shape(database, comparisonDetails[1])
+
+        colourResults = compare_image_colour(database, comparisonDetails[1])
+
+        textResults = compare_image_text(database, comparisonDetails[2])
+        
+        results_string = get_results_string(True, [shapeResults, colourResults, textResults], database)
+
+        gui.show_results(tkinter, True, [shapeResults, colourResults, textResults], database)
+        print(results_string)
+
+    else:
+        textResults = compare_text(database, comparisonDetails[2])
+
+        results_string = get_results_string(False, [textResults], database)
+        print(results_string)
+
+        gui.show_results(tkinter, False, [textResults], database)
+
+#(data_dict, isImage, [[[number, score], [number, score], [number, score]],  [],   []])
+if __name__ == '__main__':
+    main()
